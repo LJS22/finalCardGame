3c4657ec51ac3fc8a4e8078e30717b0a77e9c804
LJS22 <LJS22@users.noreply.github.com>
2020-09-17 10:33:19 +0100

    updated server file

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ const dbConnection = require("./db/connection");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-if (
-  process.env.NODE_ENV === "production" ||
-  process.env.NODE_ENV === "staging"
-) {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"));
-  });
-}
+// if (
+//   process.env.NODE_ENV === "production" ||
+//   process.env.NODE_ENV === "staging"
+// ) {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname + "/client/build/index.html"));
+//   });
+// }
+
+app.use(express.static(path.join(__dirname, "client/build")));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
